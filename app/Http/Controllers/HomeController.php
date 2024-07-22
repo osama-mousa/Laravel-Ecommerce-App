6e1234334704c032c3ec021d79cd7282a5659883
osamasayd <osamasayd141@gmail.com>
2024-07-22 15:16:06 +0300

    create HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view("layouts.master", [
+            "title" => "Home",
+            "user" => Auth::user(),
+        ]);
+    }
+}
