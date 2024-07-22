0d8f633b24081ffdb4eb371b84d13fff10ba4459
osamasayd <osamasayd141@gmail.com>
2024-07-22 15:23:03 +0300

    update endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QuestionsController;
 use App\Http\Controllers\TagsController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    $result = DB::table('catagories')->get();
-    // dd($result);
-    return view('welcome', ['catagories' => $result]);
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/tags', [TagsController::class, 'index'])->name('tags.index');
 Route::get('/tags/create', [TagsController::class, 'create'])->name('tags.create');
@@ -18,6 +17,8 @@ Route::post('/tags', [TagsController::class, 'store'])->name('tags.store');
 Route::get('/tags/{id}/edit', [TagsController::class, 'edit'])->name('tags.edit');
 Route::put('/tags/{id}', [TagsController::class, 'update'])->name('tags.update');
 Route::delete('/tags/{id}', [TagsController::class, 'destroy'])->name('tags.destroy');
+
+Route::resource('questions', QuestionsController::class);
 
 Route::get('/catagory/{catid?}', function ($catid = null) {
     // dd($catid);
@@ -31,9 +32,7 @@ Route::get('/catagory/{catid?}', function ($catid = null) {
 });
 
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 
 
 Route::get('/dashboard', function () {
