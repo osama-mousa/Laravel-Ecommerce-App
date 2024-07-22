e095174bced87ef39b86edd06583aa2581e200fe
osamasayd <osamasayd141@gmail.com>
2024-07-22 15:22:40 +0300

    adding (Auth::check())

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,8 +1,10 @@
-@extends('layout.default')
+@extends('layouts.default')
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-3">
         <h2 class="mb-4">{{ $title }}</h2>
-        <a href="/tags/create" class="btn btn-outline-dark">Create Tag</a>
+        @if (Auth::check())
+            <a href="/tags/create" class="btn btn-outline-dark">Create Tag</a>
+        @endif
     </div>
     @if (session()->has('success'))
         <div class="alert alert-success">
@@ -23,7 +25,9 @@
                 <th>NAME</th>
                 <th>CREATED AT</th>
                 <th>UPDATED AT</th>
-                <th>ACTIONS</th>
+                @if (Auth::check())
+                    <th>ACTIONS</th>
+                @endif
             </tr>
         </thead>
         <tbody>
@@ -33,38 +37,48 @@
                     <td><a href="/tags/{{ $tag->id }}/edit">{{ $tag->name }}</a></td>
                     <td>{{ $tag->created_at }}</td>
                     <td>{{ $tag->updated_at }}</td>
-                    <td>
-                        <form class="delete-form" action="/tags/{{ $tag->id }}" method="post">
-                            @csrf
-                            @method('delete')
-                            <a href="/tags/{{ $tag->id }}/edit" class="btn btn-primary btn-sm">Edit</a>
-                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                        </form>
-                    </td>
+                    @if (Auth::check())
+                        <td>
+                            <form class="delete-form" action="/tags/{{ $tag->id }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <a href="/tags/{{ $tag->id }}/edit" class="btn btn-primary btn-sm">Edit</a>
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    @endif
                 </tr>
             @endforeach
         </tbody>
     </table>
 
     <script>
-        setTimeout(function() {
-            document.querySelector('.alert').style.display = "none"
-        }, 1000);
+        
+        const showAlert = function() {
 
-        // document.querySelector('.delete-form').addEventListener('submit', function(e) {
-        //     e.preventDefault();
-        //     if (confirm('Are you sure you want to delete this item?')) {
-        //         this.submit();
-        //     }
-        // })
+            setTimeout(function() {
+                document.querySelector('.alert').style.display = "none"
+            }, 1000);
 
-        document.querySelectorAll('.delete-form').forEach(form => {
-            form.addEventListener('submit', function(e) {
-                e.preventDefault();
-                if (confirm('Are you sure you want to delete this item?')) {
-                    this.submit();
-                }
+            // document.querySelector('.delete-form').addEventListener('submit', function(e) {
+            //     e.preventDefault();
+            //     if (confirm('Are you sure you want to delete this item?')) {
+            //         this.submit();
+            //     }
+            // })
+
+            document.querySelectorAll('.delete-form').forEach(form => {
+                form.addEventListener('submit', function(e) {
+                    e.preventDefault();
+                    if (confirm('Are you sure you want to delete this item?')) {
+                        this.submit();
+                    }
+                });
             });
-        });
+        }
+
+        showAlert();
+        
+        // export {showAlert};
     </script>
 @endsection
